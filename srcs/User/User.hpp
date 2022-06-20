bb1c4b636e24374b8bfaca634862da6f782e4c98
Nhung-Ubuntu-Home <nnhung2@gmail.com>
2022-06-20 10:25:14 +0200

    Start Channel and User

--- srcs/User/User.hpp
+++ srcs/User/User.hpp
@@ -2,6 +2,7 @@
 #define USER_HPP
 
 #include "../../includes/ft_irc.hpp"
+#include "../Channel/Channel.hpp"
 
 class User
 {
@@ -9,7 +10,8 @@ class User
 
 
     private:
-
+        int                  _fd;            // is the fd of client socket
+        std::set<Channel *>  _joinedList;    // List of channels user has joined
 };
 
 #endif
