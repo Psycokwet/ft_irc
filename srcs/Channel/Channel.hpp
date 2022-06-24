e08c4e6d28b6d7f979936274d786a65aacf3890a
mida <mida@student.42.fr>
2022-06-24 14:40:39 +0200

    channel

--- srcs/Channel/Channel.hpp
+++ srcs/Channel/Channel.hpp
@@ -1,45 +1,51 @@
 #ifndef CHANNEL_HPP
-# define CHANNEL_HPP
+#define CHANNEL_HPP
 
-# include <iostream>
-# include <string>
+#include <iostream>
+#include <string>
 #include "../User/User.hpp"
 #include "../../includes/ft_irc.hpp"
 #include "../../includes/code.hpp"
+#include "../IRCServer/IRCServer.hpp"
 
 class Channel
 {
-	public:
+public:
+	Channel(const std::string &name, User *create);
+	Channel(Channel const &src);
+	virtual ~Channel();
 
-		Channel(const std::string &name, User *create);
-		Channel( Channel const & src );
-		virtual ~Channel();
+	Channel &operator=(Channel const &rhs);
 
-		Channel &		operator=( Channel const & rhs );
+	static bool check_channel_prefix(char c);
+	static bool check_channel_name(std::string const &name);
+	int addUser(User *user, std::string const &key);
 
-		static bool check_channel_prefix(char c);
-    	static bool check_channel_name(std::string const &name);
-		int addUser(User *user, std::string const &key);
+	bool isUser(User *user) const;
+	bool isInvited(User *user) const;
+	bool isKey() const;
+	bool isOp(User *user) const;
 
-		bool isUser(User *user) const;
-   		bool isInvited(User *user) const;
-    	bool isKey() const;
+	int setChannelMode(IRCServer *IRCServer, bool plus, char mode, std::string const &param);
+	std::string Channel::getChannelMode() const;
 
-	private:
+private:
+	std::string const _name; // nom du channell
+	std::set<User *> _user;	 // liste des users inscrit
+	std::string _topic;		 // sujet de la channel
 
-		std::string const _name; //nom du channell
-    	std::set<User *> _user; //liste des users inscrit
-    	std::string     _topic; //sujet de la channel
+	std::set<User *> _userInv; // liste des user invitee
 
-    	bool _userInvit;//seulement les user inscrit
-    	bool _nottopic;//no subjet
+	// channel mode
 
-    	std::set<User *> _op; //liste operateur
-    	std::string _key; //clee channel
-    	std::set<User *> _inv; //liste des user invitee
+	std::set<User *> _op; // liste operateur (o)
+	std::string _key;	  // clee channel (k)
+	bool _nottopic;		  // no subjet (t)
+	bool _invit;		  // seulement les user inscrit (i)
 
+	friend class IRCServer;
 };
 
-std::ostream &			operator<<( std::ostream & o, Channel const & i );
+std::ostream &operator<<(std::ostream &o, Channel const &i);
 
 #endif /* ********************************************************* CHANNEL_H */
