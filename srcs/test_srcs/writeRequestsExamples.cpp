c454b9aedd1c38c92fa17ab5ba3565b101f88178
Sophie <socarboni@gmail.com>
2022-06-25 16:47:35 +0200

    Updating files tests generation with thi-nguy datas and wip on grammar

--- srcs/test_srcs/writeRequestsExamples.cpp
+++ srcs/test_srcs/writeRequestsExamples.cpp
@@ -1,5 +1,9 @@
 
 #include "../util/logger.hpp"
+#include <sstream>
+#define SSTR(x) static_cast<std::ostringstream &>(            \
+					(std ::ostringstream() << std::dec << x)) \
+					.str()
 
 int main(int ac, char **av)
 {
@@ -9,13 +13,24 @@ int main(int ac, char **av)
 		return -1;
 	}
 	std::string folder_name = av[1];
-
-	gen_file_logger(folder_name + "QUIT.txt", "QUIT :Leaving\r\n");
-	gen_file_logger(folder_name + "JOIN_services.txt", "JOIN #services\r\n");
-	gen_file_logger(folder_name + "MODE.txt", "MODE #hackbbs\r\n");
-	gen_file_logger(folder_name + "WHO.txt", "WHO #hackbbs\r\n");
-	gen_file_logger(folder_name + "JOIN_hackbbs.txt", "JOIN #hackbbs\r\n");
-	gen_file_logger(folder_name + "PRIVMSG_charlie_gnarly.txt", "PRIVMSG charlie_gnarly :hello\r\n");
-	gen_file_logger(folder_name + "PRIVMSG_new_channel.txt", "PRIVMSG #new_channel :bla bla\r\n");
+	int i = 0;
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "QUIT :Leaving\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "JOIN #services\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "MODE #hackbbs\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "WHO #hackbbs\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "JOIN #hackbbs\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "PRIVMSG charlie_gnarly :hello\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "PRIVMSG #new_channel :bla bla\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "WHO #new_channel\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "PART #new_channel :I quit\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "JOIN #new_channel\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "PRIVMSG thi-nguy :hello\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "NAMES #new_channel\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "NAMES #new_channel,#other_channel\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "KICK #new_channel thi-nguy :Speaking_English\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "MODE #new_channel meo +i\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "PASS abc\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "NICK thi-nguy\r\n");
+	gen_file_logger(folder_name + SSTR(i++) + ".txt", "USER thi-nguy 0 * :realname\r\n");
 	return 0;
 }
