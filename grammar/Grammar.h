2f2f556633cc323cab683f276af7b82528843b79
Sophie <socarboni@gmail.com>
2022-06-27 18:35:52 +0200

    Byebye grammar

--- grammar/Grammar.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRAMMAR_HPP
-#define GRAMMAR_HPP
-
-#define GRAMMAR_FILE "./grammar/grammar.gram"
-#define channel_names "#channel_names"
-#define message "#message"
-#define valid_commands "#valid_commands"
-
-#endif /* ********************************************************* GRAMMAR_H */
