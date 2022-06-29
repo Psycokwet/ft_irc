ba770149dc816097cd3a84ac1d3d7389b5f4a19d
Sophie <socarboni@gmail.com>
2022-06-29 14:42:54 +0200

    There is an issue when running test, they doesn't update well.

--- srcs/IrcServer/IRCServer.hpp
+++ srcs/IrcServer/IRCServer.hpp
@@ -7,7 +7,10 @@
 
 class User;
 class Channel;
+class IRCServer;
 
+#define COMMAND_METHODS_PROTOTYPE bool (IRCServer::*)(t_client_ParsedCmd &, std::vector<t_clientCmd> &)
+typedef std::map<std::string, COMMAND_METHODS_PROTOTYPE> t_commands_dictionary;
 class IRCServer
 {
 public:
@@ -15,14 +18,21 @@ public:
 	~IRCServer();
 
 	void removeDisconnectUser(int fd);
-	bool processCommand(t_client_ParsedCmd command, std::vector<t_clientCmd> &respQueue);
+	bool processCommand(t_client_ParsedCmd parsed_command, std::vector<t_clientCmd> &respQueue);
 	int getVictim();
+
+	static t_commands_dictionary _commandsDictionnary;
+	static t_commands_dictionary initCommandsDictionnary();
 
 private:
 	std::map<int, User *> _users;				// Users list
 	std::map<std::string, Channel *> _channels; // Channels list
 	std::string const _serverPassword;
 	int _fdToKill; // Fd of user killed by operater
+
+	// COMMANDS definitions
+	bool ignore_command(t_client_ParsedCmd &parsed_command, std::vector<t_clientCmd> &respQueue);  // please add needed arg as specified in "command_method"
+	bool example_command(t_client_ParsedCmd &parsed_command, std::vector<t_clientCmd> &respQueue); // please add needed arg as specified in "command_method"
 
 	void removeUserFromAllChannels(User *user);
 };
---
+++ srcs/test_srcs/mainDispatchTest.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mainParserTest.cpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/19 22:03:00 by scarboni          #+#    #+#             */
+/*   Updated: 2022/05/09 17:20:34 by scarboni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../util/parse.hpp"
+#include "../IrcServer/IRCServer.hpp"
+#include <ctype.h>
+#include <iostream>
+#include <sstream>
+#include <cstring>
+#include <list>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
+int main(int ac, char **av)
+{
+	(void)av;
+	if (ac != 2)
+	{
+		std::cout << "I'm only a request parser for test, what do you expect by not giving me any file name to read from?" << std::endl;
+		return 0;
+	}
+
+	std::ifstream ifs(av[1]);
+
+	std::string tmp;
+	std::string buff;
+	while (std::getline(ifs, tmp))
+	{
+		buff += tmp + "\n";
+	}
+
+	IRCServer *myIrc = new IRCServer("password");
+	lazyParsedType *lad = LazyRequestParser(buff);
+	if (!lad)
+	{
+		std::cout << RED << "FAILURE\n"
+				  << RESET;
+		return -1;
+	}
+	std::cout << BLUE << "Parsed, original:\n"
+			  << GREEN
+			  << buff
+			  << RESET;
+	print_lazyrequest(std::cout, *lad);
+	std::vector<t_clientCmd> empty;
+	t_client_ParsedCmd testDatas = std::make_pair(0, lad);
+	myIrc->processCommand(testDatas, empty);
+	delete myIrc;
+	return 0;
+}
