d4a58eb3630a889dba4dcfdb6bf45b03598c35e8
Sophie <socarboni@gmail.com>
2022-07-16 11:52:28 +0200

    PART message on disconnect

--- srcs/network/MasterServer.cpp
+++ srcs/network/MasterServer.cpp
@@ -369,9 +369,8 @@ void MasterServer::removeClient(int fdClient)
 {
 	if (_clients.find(fdClient) != _clients.end())
 	{
-
 		for (std::map<std::string, Channel *>::iterator it = _channels.begin(); it != _channels.end(); it++)
-			it->second->quit(_clients[fdClient]);
+			it->second->quit_part(this, _clients[fdClient], "PART " + it->second->getName() + " :disconnected");
 		delete _clients[fdClient];
 		_clients.erase(fdClient);
 	}
